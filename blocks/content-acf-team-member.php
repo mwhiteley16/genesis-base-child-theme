2de18309622086564be83280ff1cb64f7ee2bfce
Matt Whiteley <matt@whiteleydesigns.com>
2019-04-17 15:29:35 -0400

    Organizing Files

--- blocks/content-acf-team-member.php
+++ blocks/content-acf-team-member.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Team Member block
+ * Block
  *
  * @package      ClientName
  * @author       Matt Whiteley
@@ -8,31 +8,17 @@
  * @license      GPL-2.0+
 **/
 
-$wd_team_name = get_field( 'wd_team_name' );
-$wd_team_position = get_field( 'wd_team_position' );
-$wd_team_content = get_field( 'wd_team_content' );
-$wd_team_headshot = get_field( 'wd_team_headshot' );
-$wd_button = get_field( 'wd_button' );
+// Block editor classes
+$align_class = $block['align'] ? 'align' . $block['align'] : '';
+$block_class = $block['className'] ? $block['className'] : '';
+
+// ACF Variable custom meta
 
 ?>
 
-<div class="team-member">
+<div class="block-wrapper <?php if( $align_class ) { echo $align_class; } ?> <?php if( $block_class ) { echo $block_class; } ?>">
+     <div class="wrap">
 
-     <div class="team-member__headshot">
-          <img src="<?php echo $wd_team_headshot['url']; ?>" alt="<?php echo $wd_team_headshot['alt']; ?>">
+
      </div>
-
-     <div class="team-member__content">
-          <h2 class="team-member--name"><?php echo $wd_team_name; ?></h2>
-          <span class="team-member--position"><?php echo $wd_team_position; ?></span>
-          <div class="team-member--content">
-               <?php echo $wd_team_content; ?>
-               <?php if ( $wd_button ) { ?>
-                    <div class="team-member--button">
-                         <a class="button" href="<?php echo $wd_button['url']; ?>" target="<?php echo $wd_button['target']; ?>"><?php echo $wd_button['title']; ?></a>
-                    </div>
-               <?php } ?>
-          </div>
-     </div>
-
 </div>
