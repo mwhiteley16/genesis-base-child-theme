c10b6ee1ec21109919af6cfcd07faa6361bcac19
Matt Whiteley <matt@whiteleydesigns.com>
2019-04-17 15:52:09 -0400

    Mobile Navigation Setup

--- sections/header.php
+++ sections/header.php
@@ -3,3 +3,18 @@
           <?php the_custom_logo(); ?>
      </div>
 <?php endif; ?>
+
+<div class="mobile-navigation-icon">
+     <span></span>
+     <span></span>
+     <span></span>
+</div>
+
+<script>
+jQuery(document).ready(function() {
+     jQuery('.mobile-navigation-icon').click(function() {
+          jQuery(this).toggleClass('open');
+          jQuery('.nav-primary').slideToggle();
+     });
+})
+</script>
