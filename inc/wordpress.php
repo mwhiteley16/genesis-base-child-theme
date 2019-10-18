92ac3c3db04f26d2e3bcdd28e4decb3ae128787e
Matt Whiteley <matt@whiteleydesigns.com>
2019-10-18 14:44:33 -0400

    Hidden Field Labels

--- inc/wordpress.php
+++ inc/wordpress.php
@@ -42,5 +42,46 @@ function wd_remove_admin_bar() {
 	}
 }
 
+// Clean up menu classes
+function wd_clean_nav_menu_classes( $classes ) {
+	if( ! is_array( $classes ) )
+		return $classes;
+	foreach( $classes as $i => $class ) {
+		// Remove class with menu item id
+		$id = strtok( $class, 'menu-item-' );
+		if( 0 < intval( $id ) )
+			unset( $classes[ $i ] );
+		// Remove menu-item-type-*
+		if( false !== strpos( $class, 'menu-item-type-' ) )
+			unset( $classes[ $i ] );
+		// Remove menu-item-object-*
+		if( false !== strpos( $class, 'menu-item-object-' ) )
+			unset( $classes[ $i ] );
+		// Change page ancestor to menu ancestor
+		if( 'current-page-ancestor' == $class ) {
+			$classes[] = 'current-menu-ancestor';
+			unset( $classes[ $i ] );
+		}
+	}
+	// Remove submenu class if depth is limited
+	if( isset( $args->depth ) && 1 === $args->depth ) {
+		$classes = array_diff( $classes, array( 'menu-item-has-children' ) );
+	}
+	return $classes;
+}
+add_filter( 'nav_menu_css_class', 'wd_clean_nav_menu_classes', 5 );
+
+// Clean up post classes
+function wd_clean_post_classes( $classes ) {
+	if( ! is_array( $classes ) )
+		return $classes;
+	$allowed_classes = array(
+  		'hentry',
+  		'type-' . get_post_type(),
+   	);
+	return array_intersect( $classes, $allowed_classes );
+}
+add_filter( 'post_class', 'wd_clean_post_classes', 5 );
+
 // Add custom image sizes
 // add_image_size( 'size-name', 000, 000, true );
