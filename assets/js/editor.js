09cf058e70e23abbb8247d389f20dfe970cc0138
Matt Whiteley <matt@whiteleydesigns.com>
2019-12-26 14:40:59 -0500

    Create Core Block Partials
    Used for common customizations used over time for better file organization.

--- assets/js/editor.js
+++ assets/js/editor.js
@@ -2,9 +2,8 @@
 wp.domReady( () => {
 
      // remove default buttons styles
-	wp.blocks.unregisterBlockStyle( 'core/button', 'default' );
+	wp.blocks.unregisterBlockStyle( 'core/button', 'fill' );
 	wp.blocks.unregisterBlockStyle( 'core/button', 'outline' );
-	wp.blocks.unregisterBlockStyle( 'core/button', 'squared' );
 
      // remove default separator styles
      wp.blocks.unregisterBlockStyle( 'core/separator', 'dots')
@@ -20,6 +19,23 @@ wp.domReady( () => {
 	wp.blocks.registerBlockStyle( 'core/button', {
 		name: 'inverse',
 		label: 'Inverse',
-	} );
+	});
+
+     // add custom list styles
+     wp.blocks.registerBlockStyle( 'core/list', {
+		name: 'default',
+		label: 'Default',
+		isDefault: true,
+	});
+
+	wp.blocks.registerBlockStyle( 'core/list', {
+		name: 'two-column-list',
+		label: 'Two-Columns',
+	});
+
+     wp.blocks.registerBlockStyle( 'core/list', {
+		name: 'three-column-list',
+		label: 'Three-Columns',
+	});
 
 } );
--- assets/js/main-js.js
+++ assets/js/main-js.js
@@ -1,3 +1,2 @@
 // @codekit-prepend quiet "flickity.pkgd.min.js";
 // @codekit-prepend quiet "jquery.lazyload.js";
-// @codekit-prepend quiet "wd-slideshow.js";
