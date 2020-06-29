8dc6d5b2d3bc130656b32c3c78036e80d3ce9d51
Matt Whiteley <matt@whiteleydesigns.com>
2020-06-29 16:08:44 -0400

    Responsive Spacers / Full Width Group Block

--- assets/js/editor.js
+++ assets/js/editor.js
@@ -50,4 +50,42 @@ wp.domReady( () => {
           label: 'No Bottom Margin',
      });
 
+     // add custom spacer styles
+     wp.blocks.registerBlockStyle( 'core/spacer', {
+          name: 'responsive-small',
+          label: 'Responsive Small (100px-60px)',
+     });
+
+     wp.blocks.registerBlockStyle( 'core/spacer', {
+          name: 'responsive-medium',
+          label: 'Responsive Medium (120px-80px)',
+     });
+
+     wp.blocks.registerBlockStyle( 'core/spacer', {
+          name: 'responsive-large',
+          label: 'Responsive Large (140px-100px)',
+     });
+
+     wp.blocks.registerBlockStyle( 'core/spacer', {
+          name: 'responsive-hide',
+          label: 'Responsive Hidden',
+     });
+
+     wp.blocks.registerBlockStyle( 'core/spacer', {
+          name: 'desktop-hide',
+          label: 'Desktop Hidden',
+     });
+
+     // add custom group block styles
+     wp.blocks.registerBlockStyle( 'core/group', {
+          name: 'default',
+          label: 'Default',
+          isDefault: true,
+     });
+
+     wp.blocks.registerBlockStyle( 'core/group', {
+          name: 'full-inner-width',
+          label: 'Full Inner Width',
+     });
+
 } );
