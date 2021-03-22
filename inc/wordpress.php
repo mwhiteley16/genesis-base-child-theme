cc640128638eeefa87e59ed806e5906db7247b85
Matt Whiteley <matt@whiteleydesigns.com>
2021-03-22 15:37:32 -0400

    Remove SVG support from there. Use Safe SVG Plugin.
    More secure.

--- inc/wordpress.php
+++ inc/wordpress.php
@@ -22,42 +22,6 @@ function wd_remove_comments_allowed_tags( $defaults ) {
 }
 
 
-// Allow SVG Uploads
-add_filter( 'wp_check_filetype_and_ext', function( $data, $file, $filename, $mimes ) {
-
-     global $wp_version;
-     if ( $wp_version !== '4.7.1' ) {
-          return $data;
-     }
-
-     $filetype = wp_check_filetype( $filename, $mimes );
-
-     return [
-          'ext'             => $filetype['ext'],
-          'type'            => $filetype['type'],
-          'proper_filename' => $data['proper_filename']
-     ];
-
-}, 10, 4 );
-
-function cc_mime_types( $mimes ){
-     $mimes['svg'] = 'image/svg+xml';
-     return $mimes;
-}
-add_filter( 'upload_mimes', 'cc_mime_types' );
-
-
-function fix_svg() {
-     echo '<style type="text/css">
-          .attachment-266x266, .thumbnail img {
-               width: 100% !important;
-               height: auto !important;
-          }
-          </style>';
-}
-add_action( 'admin_head', 'fix_svg' );
-
-
 // Clean up menu classes
 function wd_clean_nav_menu_classes( $classes ) {
 	if( ! is_array( $classes ) )
